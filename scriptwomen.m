cce5b285fb8f154c7677f4a20125a8e9db467cb1
Adnan Haider <s0840844@raycroft.inf.ed.ac.uk>
2013-06-17 18:42:34 +0100

    MFF women finished

--- scriptwomen.m
+++ scriptwomen.m
@@ -1,15 +1,16 @@
-load ('orignalTestExp')
-T1=Y{4};
+load orignalTestMFCCExp
+T1=MFCC{4};
 if exist('iterator4.mat','file')==2
+    load iterator4
     j=real(i)+1;
     i=i+5;
     i=real(i);
-    if i>119
+    if i>19
         i=19;
     end
     save('iterator4.mat','i')
     part=T1(j:i,:,:);
-    clear T1 Y
+    clear T1 MFCC
     load ('MFCCtraining.mat')
     output = DynamicTimeWarp(part,MFCC);
     
