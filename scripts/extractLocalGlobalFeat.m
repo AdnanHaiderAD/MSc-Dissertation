5fb440372132a63b392bb5bce59425f5c8217296
Mustafa Haider <s0840844@sms.ed.c.uk>
2013-06-19 13:40:52 +0100

    samples resampled

--- scripts/extractLocalGlobalFeat.m
+++ scripts/extractLocalGlobalFeat.m
@@ -1,23 +1,59 @@
-function localGlobalfeatures =extractLocalGlobalFeat(data)
-[regions classes versions] = size(data);
-localGlobalfeatures =cell(regions,classes,versions,2);
+function LocGlob =extractLocalGlobalFeat(data)
+% alternative to MFCC, extracts local +global features from the tim-series
+% data.
 
-for r=1 :regions
-    for c=1 : classes
-        for v=1:versions
-            seq=data{r,c,v};
-            len =length(seq);
-            globalFeatures =zeros(len-2,2);
-            localFeatures = zeros(len-2,2);
-            for l=2:len-1
-                globalFeatures(l-1,1)=seq(l)-sum(seq(1:l));
-                globalFeatures(l-1,2)=seq(l)-sum(seq(l+1:end));
-                localFeatures(l-1,1) =seq(l)-seq(l-1);
-                localFeatures(l-1,2) =seq(l)-seq(l+1);
-            end
-            localGlobalfeatures{r,c,v,1}=globalFeatures;
-            localGlobalfeatures{r,c,v,2}=localFeatures;
-        end
+tic
+
+LocGlob= cell(1,4);
+boy = data{1};
+girl=data{2};
+men= data{3};
+women=data{4};
+
+LocGLoDataboy =extractLG(boy);
+LocGLoDatagirl=extractLG(girl);
+LocGLoDatamen = extractLG(men);
+LocGLoDatawomen =extractLG(women);
+
+LocGlob{1} =LocGLoDataboy;
+LocGlob{2} =LocGLoDatagirl;
+LocGlob{3} =LocGLoDatamen;
+LocGlob{4} =LocGLoDatawomen;
+
+function  localGLo =extractLG(data)
+
+localGLo=cell(length(data),1);
+for i=1:length(data)
+    entry =data{i};
+    class=entry{1};
+    dataPoint = entry{2};
+    localFeat = zeros(2,length(dataPoint)-2);
+    globalFeat = zeros(2,length(dataPoint)-2);
+    for j=2:length(dataPoint)-1
+        globalFeat(:,j-1)= [(dataPoint(j)-sum(dataPoint(1:j-1))) (dataPoint(j)-sum(dataPoint(j+1:end)))];
+        localFeat(:,j-1) =[ (dataPoint(j) -dataPoint(j-1))  (dataPoint(j)- dataPoint(j+1))];
+    end
+    new_data =cell(2,1);
+    new_data{1}=localFeat;
+    new_data{2}=globalFeat;
+    clear entry dataPoint
+    entry = cell(2,1);
+    entry{1} =class;
+    entry{2} = new_data;
+    localGLo{i} =entry;
+    
+    if toc>300
+       toc
+       tic
     end
 end
+end
+end
+    
+        
+    
+
+
+
+
                 
---
+++ scripts/reextract.m
@@ -0,0 +1,17 @@
+function Data =reextract(data)
+%only keep samples from one production
+boy =data{1};
+girl= data{2};
+men =data{3};
+women=data{4};
+clear data
+boy= boy(2:2:end);
+girl =girl(2:2:end);
+men= men(2:2:end);
+women=women(2:2:end);
+Data=cell(1,4);
+Data{1}=boy;
+Data{2}=girl;
+Data{3}=men;
+Data{4}=women;
+end
