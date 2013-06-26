a69216dc4f6edaf35b5784a81433b69c28b747cc
Adnan <s0840844@sms.ed.ac.uk>
2013-06-26 15:13:05 +0100

    Organized

--- scriptmenmfcc.m
+++
@@ -1,7 +0,0 @@
-load RawdataCleanedExp
-men=RawdataC{3};
-clear RawdataC
-load ReducedCleanedRawTrain
-i=fix(length(men)/4);
-
-output =DynamicTimeWarp(men(22:i),RawdataC);
--- scriptwomenmfcc.m
+++
@@ -1,7 +0,0 @@
-load RawdataCleanedExp
-women=RawdataC{4};
-clear RawdataC
-load ReducedCleanedRawTrain
-i=fix(length(women)/4);
-
-output =DynamicTimeWarp(women(32:i),RawdataC);
