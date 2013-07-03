fa05b3db0a94a2dddb47bcc4f6bf8b84091885c1
Mustafa Haider <s0840844@sms.ed.c.uk>
2013-07-03 13:48:32 +0100

    Ready for meeting

--- scripts/analysis.m
+++ scripts/analysis.m
@@ -1,12 +1,12 @@
-function [indVaccuracy,accuracy]=  analysis(data,refdata)
-%%plot an histogram  and a grapgh
+function [MisClassCount ,Counter,Accuracy]=  analysis(data,refdata)
+%%plot an histogram  and a graph
 %the histogram corresponds to the number of in-correct 
 % classifications performed by the algorithm for each class
 %The graph correpsponds to the accuracy
 count=0;
 MisClassCount =zeros(9,1);
 Counter=zeros(9,1);
-
+length(refdata)
 for i=1:length(refdata)
     entry =refdata{i};
     class =entry{1};
@@ -21,25 +21,16 @@ for i=1:length(refdata)
             count=count+1;
             %%records the number of misclassifications seen for the class so
             %far
-            histogram(count)=class;
+            
             MisClassCount(class)=MisClassCount(class)+1;
         end
     end
 end
 
 %% misclassification rate
-figure(1)
+Accuracy= 100-count/sum(Counter)*100;
+Counter=Counter-MisClassCount;
 
-hist(histogram,[1:9]);
-h = findobj(gca,'Type','patch');
-set(h,'FaceColor',[1 0.5 0],'EdgeColor','w')
-xlabel('Classes'); 
-ylabel('Number of Mis-Classifications'); 
- title('Results for the women category');
 
-%%accuracy
-output= MisClassCount./Counter*100;
-indVaccuracy = 100*ones(9,1) -output;
-accuracy =100-count/sum(Counter) *100;
 
 end
