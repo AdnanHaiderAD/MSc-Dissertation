90e1686145f96a68d37a775a88c618fffb1eef01
Mustafa Haider <s0840844@sms.ed.c.uk>
2013-07-26 11:26:44 +0100

    implementing wavelet

--- Time_seriesdatasets/patternmatch.m
+++ Time_seriesdatasets/patternmatch.m
@@ -4,22 +4,26 @@ function output = patternmatch(test,train)
 tic
 
 test_labels=test(:,1);
-testData=test(:,2:end)';
+testData=test(:,2:end);
 
 train_labels=train(:,1);
-trainData=train(:,2:end)';
+
+%% the data is now N by D matrix
+trainData=train(:,2:end);
 
 fingerprintSpace = principalcomponents(trainData);
 
-%% projection to principal subspace
-trainDataR= fingerprintSpace'* trainData;
-testDataR= fingerprintSpace'* testData;
 
+%% projection to principal subspace
+trainDataR= (fingerprintSpace'* trainData')';
+testDataR= (fingerprintSpace'* testData')';
 output=zeros(1,length(test_labels));
 
+
+%%perform 1 nearest neighbour
 for i=1 :length(test_labels)
-    match =nearest_neighbours(testDataR(:,i),trainDataR,train_labels);
-    if match ~=test_labels(i)
+    match =nearest_neighbours(testDataR(i,:),trainDataR,train_labels);
+    if match ==test_labels(i)
         output(i)=1;
     end
     
@@ -27,14 +31,15 @@ end
 
 
 function match= nearest_neighbours(sample,data,train_labels)
-%% performs 1 nearest neigbour search.
-    [dim, samp]= size(data);
-    nearestN=0;
+%% conducts 1 nearest neigbour search.
+    [samp, dim]= size(data);
+    nearestN=NaN;
     min_dist=Inf;
     for j=1 :samp
-        dist= sum ((sample-data(:,j)).^2);
+        dist= sum ((sample-data(j,:)).^2);
         if dist<min_dist
             nearestN = train_labels(j);
+            min_dist=dist;
         end
     end
     match =nearestN;
