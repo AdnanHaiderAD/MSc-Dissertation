7cf0953f758fd2fe7d66dc901bc6bf6a96124f72
Mustafa Haider <s0840844@sms.ed.c.uk>
2013-07-27 20:47:53 +0100

    adkadka

--- Time_seriesdatasets/patternmatch.m
+++ Time_seriesdatasets/patternmatch.m
@@ -16,14 +16,14 @@ trainData=train(:,2:end);
 function result=wavedecom(data)
 %% performs wavelet decomposition
   [samp ,dim]=size(data);
-  result=zeros(samp,1+ fix(dim/2));
+  %result=zeros(samp,1+ fix(dim/2));
   %result=zeros(samp,1+ dim);
-  
+  result=zeros(samp,1+3740);
   result(:,1)=data(:,1);
   for k=1 :samp
     sample= data(k,2:end);
-    [C,L] =wavedec(sample,13,'Haar');
-    result(k,2:end) = C(1:L(end-1));
+    [C,L] =wavedec(sample,14,'Haar');
+    result(k,2:end) = C(1:L(end-3));
     %result(k,2:end)=C(1:dim);
   end
 end
@@ -31,23 +31,23 @@ end
 function result=fourierdecom(data)
     
 [samp ,dim]=size(data);
-%result=zeros(samp,1+ fix(dim/2));
-result=zeros(samp,1+ dim);
+result=zeros(samp,1+ fix(dim/2));
+%result=zeros(samp,1+ dim);
   
 result(:,1)=data(:,1);
 for k=1 :samp
     sample= data(k,2:end);
     fouriercoeff = abs(fft(sample));
     
-    %result(k,2:end) = fouriercoeff(1:fix(dim/2));
-    result(k,2:length(fouriercoeff)+1)=fouriercoeff;
+    result(k,2:end) = fouriercoeff(1:fix(dim/2));
+    %result(k,2:length(fouriercoeff)+1)=fouriercoeff;
 end
 end
 time=toc
 tic
 %%perform wavelet decomposition: feature extraction
-%testData=wavedecom(testData);
-%trainData=wavedecom(trainData);
+testData=wavedecom(testData);
+trainData=wavedecom(trainData);
 
 %% perform fourier transfor,
 %testData=fourierdecom(testData);
@@ -59,6 +59,7 @@ fingerprintSpace = principalcomponents(trainData);
 %% projection to principal subspace
 trainDataR= (fingerprintSpace'* trainData')';
 testDataR= (fingerprintSpace'* testData')';
+
 output=zeros(1,length(test_labels));
 time=time+toc
 tic
@@ -81,7 +82,7 @@ end
 
 time =time+toc
 tic
-%%perform 1 nearest neighbour
+perform 1 nearest neighbour
 for i=1 :length(test_labels)
     if toc>300
         time=time+toc
@@ -91,8 +92,9 @@ for i=1 :length(test_labels)
     if match ==test_labels(i)
         output(i)=1;
     end
-    
 end
+% output= DynamicTimeWarping(testDataR(:,2:end),trainDataR(:,2:end),testDataR(:,1),trainDataR(:,1));   
+
 
 
 time=time+toc
